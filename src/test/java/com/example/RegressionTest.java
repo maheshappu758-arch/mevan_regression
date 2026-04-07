d5eeddb28e9d04c50faa0c1e6b17283d3daec343
maheshappu758-arch <maheshappu758@gmail.com>
2026-04-07 21:57:43 +0530

    Create RegressionTest.java

---
+++ src/test/java/com/example/RegressionTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegressionTest {
+
+    @Test
+    void sampleTest() {
+        int result = 1 + 1;
+        assertEquals(2, result);
+    }
+}
